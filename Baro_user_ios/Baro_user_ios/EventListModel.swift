cd2397390dd000a1f9876fcd4ea90b19d5c909f8
kiiiimsj <kiiiimsj@naver.com>
2020-10-19 20:12:07 +0900

    1

---
+++ Baro_user_ios/Baro_user_ios/EventListModel.swift
@@ -0,0 +1,15 @@
+//
+//  EventListModel.swift
+//  Baro_user_ios
+//
+//  Created by 김성재 on 2020/10/19.
+//
+
+import Foundation
+
+import Foundation
+
+struct EventListModel {
+    var event_id = "이벤트아이디"
+    var event_image = "이벤트이미지"
+}
