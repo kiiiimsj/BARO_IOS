5d831ccf6d7f88289ee741fb0c804d3e0a1ff9f7
hty <hty15973@naver.com>
2020-10-20 21:16:19 +0900

    ddddddddddddddd

--- Baro_user_ios/Baro_user_ios/AboutStore.swift
+++ Baro_user_ios/Baro_user_ios/AboutStore.swift
@@ -16,6 +16,7 @@ class AboutStore : UIViewController {
     private let netWork = CallRequest()
     private let urlMaker = NetWorkURL()
     private var menus = [Menu]()
+    private var StoreInfo = StoreInfoModel()
     private var categories = Dictionary<Int,String>()
     public var union = [String : [Menu]]()
     public var first = ["메뉴 정보","가게 정보"]
@@ -26,8 +27,8 @@ class AboutStore : UIViewController {
         super.viewDidLoad()
         storeInfoManager.netWork = netWork
         storeInfoManager.urlMaker = urlMaker
-        contollers.append(storeInfoManager)
         contollers.append(storeMenuManager)
+        contollers.append(storeInfoManager)
         netWork.get(method: .get, url: urlMaker.categoryURL + "?store_id=1") { (json) in
             if json["result"].boolValue{
                 for item in json["category"].array!{
@@ -54,6 +55,24 @@ class AboutStore : UIViewController {
                         self.FirstBar.delegate = self
                         self.FirstBar.dataSource = self
                         self.FirstBar.reloadData()
+                        self.netWork.get(method: .get, url: "http://15.165.22.64:8080/StoreFindById.do?store_id=1") {(json) in
+                            self.StoreInfo.store_id = json["store_id"].intValue
+                            self.StoreInfo.store_opentime = json["store_opentime"].stringValue
+                            self.StoreInfo.store_latitude = json["store_latitude"].doubleValue
+                            self.StoreInfo.store_closetime = json["store_closetime"].stringValue
+                            self.StoreInfo.store_daysoff = json["store_daysoff"].stringValue
+                            self.StoreInfo.message = json["message"].stringValue
+                            self.StoreInfo.result = json["result"].boolValue
+                            self.StoreInfo.store_phone = json["store_phone"].stringValue
+                            self.StoreInfo.store_longitude = json["store_longitude"].doubleValue
+                            self.StoreInfo.store_name = json["store_name"].stringValue
+                            self.StoreInfo.store_location = json["store_location"].stringValue
+                            self.StoreInfo.type_code = json["type_code"].stringValue
+                            self.StoreInfo.store_image = json["store_image"].stringValue
+                            self.StoreInfo.is_open = json["is_open"].stringValue
+                            self.StoreInfo.store_info = json["store_info"].stringValue
+                             
+                        }
                     }else{
                         
                         
@@ -67,11 +86,29 @@ class AboutStore : UIViewController {
         actionToSelectedCell(indexPath: indexPath)
     }
     func actionToSelectedCell(indexPath : IndexPath){
-        setContainerViewController(index: indexPath.row)
+        switch indexPath.row {
+                case 0:
+                    setContainerViewController(storyboard: "AboutStore", viewControllerID: "StoreMenuController",index: indexPath.row)
+                case 1:
+                    setContainerViewController(storyboard: "AboutStore", viewControllerID: "StoreInfoController",index: indexPath.row)
+                default:
+                    print("-touchUP\(indexPath.row)-")
+                }
         }
-    func setContainerViewController(index : Int){
-        let VC = contollers[index]
-        print("\(index)")
+    func setContainerViewController(storyboard: String, viewControllerID: String,index : Int){
+        let storyboard = UIStoryboard(name: storyboard, bundle: nil)
+        let VC = storyboard.instantiateViewController(withIdentifier: viewControllerID)
+//        let VC = contollers[index]
+        switch index {
+        case 0:
+            (VC as! StoreMenuController).categories = self.categories
+            break
+        case 1:
+            (VC as! StoreInfoController).StoreInfo = self.StoreInfo
+            break
+            
+        default: break
+        }
             self.addChild(VC)
             FirstPage.addSubview((VC.view)!)
             VC.view.frame = FirstPage.bounds
@@ -93,4 +130,22 @@ extension AboutStore : UICollectionViewDelegate,UICollectionViewDataSource{
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: (view.frame.width / CGFloat(first.count)*0.97),height: FirstBar.frame.height)
     }
+}
+
+struct StoreInfoModel {
+    var store_id = 0
+    var store_opentime = ""
+    var store_info = ""
+    var store_latitude = 0.0
+    var store_closetime = "22:00"
+    var store_daysoff = "매주 월, 화 휴무"
+    var message = "가게 정보 가져오기 성공."
+    var result = false
+    var store_phone = "01000000000"
+    var store_longitude = 0.0
+    var store_name  = "test cafe"
+    var store_location = "서울특별시 테스트구 테스트동 테스트로 111 테스트빌딩 2층"
+    var type_code = "CAFE"
+    var store_image = "test_cafe1.png"
+    var is_open = "N"
 }
--- Baro_user_ios/Baro_user_ios/StoreInfoController.swift
+++ Baro_user_ios/Baro_user_ios/StoreInfoController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class StoreInfoController : UIViewController{
     
-    public var netWork : CallRequest!;
-    public var urlMaker : NetWorkURL!;
-    
+    public var netWork : CallRequest!
+    public var urlMaker : NetWorkURL!
+    public var StoreInfo = StoreInfoModel()
+    @IBOutlet weak var storeIntroduce: UILabel!
+    @IBOutlet weak var openTime: UILabel!
+    @IBOutlet weak var holiday: UILabel!
+    @IBOutlet weak var phonenumber: UILabel!
+    @IBOutlet weak var location: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .black
+        view.backgroundColor = .white
+        storeIntroduce.text = StoreInfo.store_info
+        openTime.text = StoreInfo.store_opentime + " ~ " + StoreInfo.store_closetime
+        holiday.text = StoreInfo.store_daysoff
+        phonenumber.text = StoreInfo.store_phone
+        location.text = StoreInfo.store_location
     }
 }
