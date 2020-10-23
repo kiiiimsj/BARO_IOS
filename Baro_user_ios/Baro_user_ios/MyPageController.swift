5316f6d3bbf0cadce420f6a00f69052e15712bd5
LeeSeungJae <sky_battle@naver.com>
2020-10-23 14:00:22 +0900

    update mypage

--- Baro_user_ios/Baro_user_ios/MyPageController.swift
+++ Baro_user_ios/Baro_user_ios/MyPageController.swift
@@ -15,15 +15,15 @@ class MyPageController : UIViewController {
         super.viewDidLoad()
         setUserName()
     }
+    
     func setUserName() {
-        let user_name = "\(UserDefaults.standard.value(forKey: "user_email"))"
+        let user_name = "\(UserDefaults.standard.value(forKey: "user_name") as! String)"
         if user_name != "" {
-          //userName.text = "\(user_name)"
+            userName.text = "\(user_name)"
         }
-        let user_email = "\(UserDefaults.standard.value(forKey: "user_email"))"
+        let user_email = "\(UserDefaults.standard.value(forKey: "user_email") as! String)"
         if user_email != "" {
-            //userEmail.text = "\(user_email)"
-
+            userEmail.text = "\(user_email)"
         }
     }
 }
