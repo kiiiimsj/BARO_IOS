63ca8faee4678af06134e7dcb797409dc157f3da
LeeSeungJae <sky_battle@naver.com>
2020-10-27 20:22:28 +0900

    update BottomNavigation

---
+++ Baro_user_ios/Baro_user_ios/UserInterface/BottomTabBarController.swift
@@ -0,0 +1,15 @@
+//
+//  BottomTapBarControllerViewController.swift
+//  Baro_user_ios
+//
+//  Created by yUsiL on 2020/10/27.
+//
+
+import UIKit
+
+class BottomTabBarController: UITabBarController, UITabBarControllerDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
