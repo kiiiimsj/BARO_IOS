c76edf8e87f02f498c0b2dc2d6a77f236d75bcfd
hty <hty15973@naver.com>
2020-11-06 22:22:21 +0900

    ㅇㅇㅇㅇㅇ

---
+++ Baro_user_ios/Baro_user_ios/Model/AlertModel.swift
@@ -0,0 +1,15 @@
+//
+//  AlertModel.swift
+//  Baro_user_ios
+//
+//  Created by . on 2020/11/06.
+//
+
+import Foundation
+
+struct AlertModel {
+    var alert_id = 0
+    var alert_title = ""
+    var alert_content = ""
+    var alert_startdate = ""
+}
