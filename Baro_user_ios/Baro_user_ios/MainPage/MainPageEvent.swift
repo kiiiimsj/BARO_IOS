21f936dd56026d44e2e777e226d32c4b2ad56468
hty <hty15973@naver.com>
2020-11-11 21:51:24 +0900

    ㅇㅇㅇ

--- Baro_user_ios/Baro_user_ios/MainPage/MainPageEvent.swift
+++ Baro_user_ios/Baro_user_ios/MainPage/MainPageEvent.swift
@@ -12,9 +12,21 @@ protocol CellDelegateEvent: class {
 }
 
 class MainPageEvent : UICollectionViewCell {
-    
+    var rightSwipe : UISwipeGestureRecognizer {
+        let swipe = UISwipeGestureRecognizer()
+        swipe.direction = .right
+        swipe.addTarget(self, action: #selector(swipe(recognizer:)))
+        return swipe
+    }
+    var leftSwipe : UISwipeGestureRecognizer {
+        let swipe = UISwipeGestureRecognizer()
+        swipe.direction = .left
+        swipe.addTarget(self, action: #selector(swipe(recognizer:)))
+        return swipe
+    }
     var delegateEvent : CellDelegateEvent?
-    
+    var a = 0
+    var indexPaths = [IndexPath]()
     var eventList = [EventListModel]()
     let networkModel = CallRequest()
     let networkURL = NetWorkURL()
@@ -24,7 +36,6 @@ class MainPageEvent : UICollectionViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        
         collectionView.delegate = self
         collectionView.dataSource = self
         
@@ -39,9 +50,25 @@ class MainPageEvent : UICollectionViewCell {
                 self.eventList.append(eventModel)
             }
             self.collectionView.reloadData()
+//            DispatchQueue.global().async {
+//
+//                while true {
+//                    Thread.sleep(forTimeInterval: 5)
+//                    print("zzz",self.a)
+//
+//                    DispatchQueue.main.sync {
+//                        self.collectionView.scrollToItem(at: self.indexPaths[self.a], at: .right, animated: true)
+//                        self.a += 1
+//                    }
+//                    if self.a >= self.eventList.count {
+//                        self.a = 0
+//                    }
+//                }
+//            }
         }
         
     }
+
 }
 
 extension MainPageEvent : UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
@@ -54,9 +81,16 @@ extension MainPageEvent : UICollectionViewDelegate, UICollectionViewDataSource, 
         let event = eventList[indexPath.item]
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MainPageEventCell", for: indexPath) as! MainPageEventCell
         cell.eventImage.kf.setImage(with: URL(string: "http://3.35.180.57:8080/ImageEvent.do?image_name=" + event.event_image))
-        
+        cell.backgroundColor = .purple
         //cell클릭시
         cell.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(tap(_:))))
+        if indexPath.item == 0 {
+            cell.addGestureRecognizer(rightSwipe)
+            cell.eventImage.addGestureRecognizer(rightSwipe)
+        }else if indexPath.item == eventList.count - 1 {
+            cell.addGestureRecognizer(leftSwipe)
+            cell.eventImage.addGestureRecognizer(leftSwipe)
+        }
         
         return cell
     }
@@ -74,5 +108,24 @@ extension MainPageEvent : UICollectionViewDelegate, UICollectionViewDataSource, 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: 400, height: 200)
     }
-    
+    @objc func swipe(recognizer : UISwipeGestureRecognizer){
+        switch recognizer.direction {
+        case .right:
+            print("rightDirection",eventList.count - 1)
+            self.collectionView.scrollToItem(at: IndexPath(item: eventList.count-1, section: 0) , at: .right, animated: true)
+        case .left:
+            print("leftDirection")
+            self.collectionView.scrollToItem(at: IndexPath(item: 0, section: 0), at: .right, animated: true)
+        default:
+            print("?????")
+        }
+    }
+    @objc func handleSwipeRightGesture(_ recognizer: UISwipeGestureRecognizer) {
+        print("This swipe is right")
+//        self.collectionView.scrollToItem(at: self.indexPaths[eventList.count-1], at: .right, animated: true)
+    }
+    @objc func handleSwipeLeftGesture(_ recognizer: UISwipeGestureRecognizer) {
+        print("This swipe is left")
+    }
 }
+
