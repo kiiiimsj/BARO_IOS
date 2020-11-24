eb7274e99e04bc4d9a564daac2a3373ab1d32803
hty <hty15973@naver.com>
2020-11-24 19:12:01 +0900

    ㅇㄹㅁㄴㄹㅇ
    ㅇㅇㅇ

--- Baro_user_ios/Baro_user_ios/Basket/BasketNonEssentialCell.swift
+++ Baro_user_ios/Baro_user_ios/Basket/BasketNonEssentialCell.swift
@@ -12,4 +12,10 @@ class BasketNonEssentialCell: UICollectionViewCell {
     @IBOutlet weak var extra_price: UILabel!
     @IBOutlet weak var extra_count: UILabel!
     @IBOutlet weak var extra_name: UILabel!
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        extra_price.text = ""
+        extra_count.text = ""
+        extra_name.text = ""
+    }
 }
