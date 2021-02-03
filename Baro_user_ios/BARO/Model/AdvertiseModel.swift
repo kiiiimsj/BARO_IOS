e7e42387a5f054acef5bad1bb370fb97698845fe
hty <hty15973@naver.com>
2021-02-03 16:43:50 +0900

    qwer

---
+++ Baro_user_ios/BARO/Model/AdvertiseModel.swift
@@ -0,0 +1,15 @@
+//
+//  AdvertiseModel.swift
+//  BARO
+//
+//  Created by . on 2021/02/02.
+//
+
+import Foundation
+
+class AdvertiseModel {
+    var event_id = 0
+    var result = false
+    var event_image = ""
+    var message = ""
+}
--- Baro_user_ios/BARO/NewMainPage/NewMainPageController.swift
+++ Baro_user_ios/BARO/NewMainPage/NewMainPageController.swift
@@ -152,6 +152,21 @@ class NewMainPageController: UIViewController, CLLocationManagerDelegate {
                 issueLabelsHeightConstraint.isActive = true
             }
         }
+        let calendar = Calendar.current
+        let today = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        dateFormatter.timeZone = NSTimeZone(name: "KST") as TimeZone?
+        let savedTime = dateFormatter.date(from: UserDefaults.standard.string(forKey: "notTodayDate")!)
+        if calendar.compare(today, to: savedTime!, toGranularity: .day) == .orderedSame {
+            
+        } else {
+            let vc = UIStoryboard.init(name: "MainPage", bundle: nil).instantiateViewController(withIdentifier: "AppStartDialog") as! AppStartDialog
+            vc.modalPresentationStyle = .overFullScreen
+            vc.modalTransitionStyle = .crossDissolve
+            self.present(vc, animated: false, completion: nil)
+        }
+        
     }
     
     func getMyLocation(_ longitude : String, _ latitude :String) {
