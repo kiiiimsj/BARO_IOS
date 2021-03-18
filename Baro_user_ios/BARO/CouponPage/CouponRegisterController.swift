95e04f5a9b2a6d1d36c85d301f6ac6e99ec84d8f
hty <hty15973@naver.com>
2021-03-18 20:06:29 +0900

    1234

--- Baro_user_ios/BARO/CouponPage/CouponRegisterController.swift
+++ Baro_user_ios/BARO/CouponPage/CouponRegisterController.swift
@@ -10,7 +10,7 @@ import UIKit
 class CouponRegisterController: UIViewController {
     var titleText : String?
     var messageText : String?
-    
+    @IBOutlet weak var background: UIView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var message: UILabel!
     @IBOutlet weak var okayBtn: UIButton!
@@ -22,5 +22,13 @@ class CouponRegisterController: UIViewController {
         super.viewDidLoad()
         titleLabel.text = titleText
         message.text = messageText
+        okayBtn.layer.cornerRadius = 5
+        okayBtn.layer.borderWidth = 1
+        okayBtn.layer.borderColor = UIColor.baro_main_color.cgColor
+        okayBtn.layer.masksToBounds = true
+        background.layer.cornerRadius = 5
+        background.layer.borderColor = UIColor.baro_main_color.cgColor
+        background.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
+        background.layer.masksToBounds = true
     }
 }
